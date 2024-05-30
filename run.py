8bc9694e6b700f4ad3a97968704904b01f94a23e
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-05-30 15:58:55 -0300

    Update run.py

--- run.py
+++ run.py
@@ -304,7 +304,7 @@ with st.expander("Clique para saber mais sobre os Agentes Experts Geomaker."):
     st.write("4. **Avaliação e aprimoramento contínuo:** Utilizando o Rational Agent Generator (RAG), garantimos que as respostas dos especialistas sejam sempre as melhores, mantendo um padrão de excelência em todas as interações.")
     st.write("5. **Desenvolvimento profissional e acadêmico:** Professores podem encontrar recursos e orientações para melhorar suas práticas de ensino, enquanto pesquisadores podem obter insights valiosos para suas investigações.")
     st.write("6. **Inovação e tecnologia educacional:** Nossa plataforma incorpora as mais recentes tecnologias para proporcionar uma experiência educacional moderna e eficiente.")
-
+    st.image("fluxograma agente 4.png")
 
 
 st.markdown("<hr>", unsafe_allow_html=True)
@@ -375,7 +375,7 @@ st.markdown("<h2 style='text-align: center;'>Manual de uso básico.</h2>", unsaf
 
 def expander(title: str, content: str, icon: str):
     with st.expander(title): 
-        st.image("fluxograma agente 4.png")
+        
         st.markdown(f'<img src="{icon}" style="vertical-align:middle"> {content}', unsafe_allow_html=True)
 
 # Conteúdo do manual de uso
